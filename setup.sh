b47e757f8686a82dc60d7f91b4e85480259e5677
David Weterings <david.weterings@gmail.com>
2015-06-23 23:16:25 +0200

    Done with compose_demo

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ apt-get install -y python-setuptools
 easy_install pip
 pip install docker-compose
 pip install requests
+mkdir /vagrant/code/compose_demo/data
+chmod -R 777 /vagrant/code/compose_demo/data
 
 # Allow incomming connections to port 8000
 #iptables -A IN_public_allow -p tcp --dport 8000 -j ACCEPT
